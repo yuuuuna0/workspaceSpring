3340b2574c5a2ae5b19b9c9373adbd1d7a65f9f5
yuuuuna0 <jyn9306@gmail.com>
2023-02-21 17:45:45 +0900

    ..

--- reflection_annotation/src/com/itwill/guest/ReflectionAnnotationGuestServiceMain.java
+++ reflection_annotation/src/com/itwill/guest/ReflectionAnnotationGuestServiceMain.java
@@ -41,7 +41,9 @@ public class ReflectionAnnotationGuestServiceMain {
 				}
 			}
 		}
-
+		System.out.println(applicationContext);
+		
+		
 		System.out.println("--------@AutoWire setter method호출--------------");
 		Iterator<String> beanIdIterator= applicationContext.keySet().iterator();
 		while (beanIdIterator.hasNext()) {
