a17a99c152b9da0f45d84d9aee92b365b1180e2b
yuuuuna0 <jyn9306@gmail.com>
2023-02-28 20:02:30 +0900

    ..

--- spring_web_application/src/main/java/com/itwill/controller/HelloAnnotationMultiRequestMappingController.java
+++ spring_web_application/src/main/java/com/itwill/controller/HelloAnnotationMultiRequestMappingController.java
@@ -19,6 +19,9 @@ public class HelloAnnotationMultiRequestMappingController {
 		return "forward:/WEB-INF/views/hello5.jsp";
 	}
 	
+	/*
+	 * redirect는 url을 물고물고 넘어가기 때문에 최종 url:/hello_redirected_servlet
+	 */
 	@RequestMapping("/hello_redirect_servlet")
 	public String hello_redirect_servlet() {
 		return "redirect:hello_redirected_servlet";
@@ -28,6 +31,9 @@ public class HelloAnnotationMultiRequestMappingController {
 		return "forward:/WEB-INF/views/hello_redirected_servlet.jsp";
 	}
 	
+	/*1. 
+	 * forward는 요청한 url로 데이터만 넘기기 때문에 최종 url:/hello_servlet_forward
+	 */
 	@RequestMapping("hello_servlet_forward")
 	public String hello_servlet_forward() {
 		return "forward:hello_servlet_forwarded";	//.jsp가 있으며 dispatcherServlet으로 안 들어감
