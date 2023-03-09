97529d852643dd594097b5edaa64a8c04981e0b7
yuuuuna0 <jyn9306@gmail.com>
2023-03-09 17:26:36 +0900

    ..

--- javascript_ajax/WebContent/ajaxRequest.js
+++ javascript_ajax/WebContent/ajaxRequest.js
@@ -1,31 +1,19 @@
-var xhr=null;
 
-function sendRequest(url,params,callback,method) {
-	/*
-	1. XMLHttpRequest객체생성
-	*/
-	xhr = new XMLHttpRequest();		
-	/*
-	2. 요청속성설정 
-	*/
-	var httpMethod = method ? method : 'GET';
-	if(httpMethod!='GET' && httpMethod!='POST'){
-		httpMethod='GET';
-	}
-	var httpParams = params ? params:null;
-	var httpUrl=url;
+function ajaxRequest(method,url,params,callbackFunction){
+	//params가 뭐지?
 	
-	if(httpMethod=='GET' && httpParams !=null){
-		httpUrl=httpUrl+"?"+httpParams;
-	}
-	/*
-	응답이 도착했을때 실행되는 콜백함수등록(이벤트프로퍼티에함수등록)
-	*/
-	xhr.onreadystatechange=callback;
-	xhr.open(httpMethod,httpUrl);
-	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	/*
-	3.요청
-	*/
-	xhr.send((httpMethod=='POST')? httpParams:null);		
+	let xhr= new XMLHttpRequest();
+	method= method ? method:'GET';
+	method=(method!='GET' && method!='POST')? 'GET':method;
+	
+	params = params ? params:null;
+	url = (method=='GET' && params!=null) ? url+"?"+params : url;
+	
+	xhr.open(method,url,true);
+	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xhr.onload=function(){
+		callbackFunction(xhr);
+	};
+	xhr.send((method=='POST')? params:null);
+	
 }
