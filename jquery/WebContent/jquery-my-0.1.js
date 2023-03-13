0bfa381a3c39806464835e1fe98ecb2971d3cab2
yuuuuna0 <jyn9306@gmail.com>
2023-03-13 18:05:57 +0900

    ..

--- jquery/WebContent/jquery-my-0.1.js
+++ jquery/WebContent/jquery-my-0.1.js
@@ -32,11 +32,47 @@ window.jQuery=function(arg){
 			}
 		}
 		return jqeuryWrapperObject;
+		
 	}else if(typeof arg=='function'){
 		//인자로 대입된 함수를 DOM트리생성 직후에 호출되도록 window.onload이벤트 프로퍼티에 등록
 		window.addEventListener('load',arg);
 	}else if(typeof arg=='object'){
-		
+		/*
+		표준객체(Element,document,window,....)
+		*/
+		let elementNodeList=[];
+		elementNodeList.push(arg);
+		let jqueryWrapperObject={
+			'elementNodeList':elementNodeList,
+			'css':function(propertyName,propertyValue){
+				for(let i=0;i<this.elementNodeList.length;i++){
+					this.elementNodeList[i].style.cssText
+					+=`${propertyName}:${propertyValue};`;
+				}
+				return this;
+			},
+			'text':function(textArg){
+				if(textArg){
+					//set text
+					for(let i=0;i<this.elementNodeList.length;i++){
+						//this.elementNodeList[i].innerHTML=textArg;
+						this.elementNodeList[i].firstChild.nodeValue=textArg;
+					}
+					return this;
+				}else if(textArg==undefined){
+					//get text
+					let returnText="";
+					for(let i=0;i<this.elementNodeList.length;i++){
+						//this.elementNodeList[i].innerHTML=textArg;
+						returnText += this.elementNodeList[i].firstChild.nodeValue;
+					}
+					return returnText;
+				}
+				
+			}
+				
+		}
+		return jqueryWrapperObject;
 	}
 }
 /*************** jQuery global function *************************** */
